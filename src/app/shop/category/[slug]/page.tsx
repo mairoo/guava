8823673403e902e77ab16d86e945c78f4f1ebd80
mairoo <jonghwa@pincoin.co.kr>
2024-12-05 00:31:49 +0900

    category markdown

--- src/app/shop/category/[slug]/page.tsx
+++ src/app/shop/category/[slug]/page.tsx
@@ -1,7 +1,9 @@
-import { DottedList, FlexColumn } from '@/components/layout';
+import { FlexColumn } from '@/components/layout';
 import { ProductGrid, ProductItemBuy } from '@/components/product';
+import { Card } from '@/components/ui/card';
 import Link from 'next/link';
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 const bestsellers = [
   {
@@ -42,32 +44,23 @@ const bestsellers = [
   },
 ];
 
+const warningContent = `
+* 한국 구글플레이스토어의 게임과 상품만 구매할 수 있습니다.
+* 지메일 계정의 국가 설정을 대한민국으로 해야 충전 및 결제할 수 있습니다.
+* 일일 충전한도는 50만원입니다.
+* **구글코리아는 국내법을 따르지 않고 취소/환불을 지원하지 않아 계정 오류 발생 등 어떤 경우에도 환불 처리되지 않습니다.**
+* **현재 등록할 수 없는 카드라는 오류 또는 사용 후 게임 내 아이템 충전 시 구글에서 추가 정보를 요구하는 경우가 발생해도 절대 환불 처리가 안 됩니다.**
+* **구글은 이의제기를 해도 거절되면 그 이유를 알려주지도 않고 계속 거절하기 때문에 어떠한 대응도 안 됩니다.**
+`;
+
 const CategoryDetailPage = () => {
   return (
     <FlexColumn spacing={2} marginY={2}>
-      <DottedList
-        indent={2}
-        border="border border-yellow-200"
-        rounded={true}
-        backgroundColor="bg-yellow-50"
-      >
-        <div>
-          다른 사람으로부터 상품권 구매로 일부 또는 전체 금액을 입금 받기로
-          했습니까?
+      <Card className="w-full border border-yellow-200 bg-yellow-50 shadow-none p-1">
+        <div className="prose prose-sm max-w-none">
+          <ReactMarkdown>{warningContent}</ReactMarkdown>
         </div>
-        <div>
-          상품권 일부 또는 전체를 대리구매 하여 카카오톡 등 메신저로 다른
-          사람에게 주기로 했습니까?
-        </div>
-        <div>
-          네이트온/카카오톡 등 메신저에서 지인이 급한 돈이 필요하다고 상품권을
-          요구했습니까?
-        </div>
-        <div>
-          중고나라 또는 번개장터에서 물품대금을 현금 대신 상품권으로 요구
-          받았습니까?
-        </div>
-      </DottedList>
+      </Card>
       <ProductGrid gap={2} py={0}>
         {bestsellers.map((product) => (
           <Link
