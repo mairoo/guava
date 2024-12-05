2fd8643b53d9b0c9209c2550075b44183735d5db
mairoo <jonghwa@pincoin.co.kr>
2024-12-05 22:48:49 +0900

    comments

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,5 +1,9 @@
 import { useEffect, useState } from 'react';
 
+// useMediaQuery 사용을 최소화
+// - hydration 불일치 가능성
+// - layout shift 발생 가능성
+// - 추가 js 의존성
 export function useMediaQuery(query: string) {
   const [matches, setMatches] = useState(false);
   const [mounted, setMounted] = useState(false);
