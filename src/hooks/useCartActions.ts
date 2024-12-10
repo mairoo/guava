b54164b3a1e8dcb60fdde2c6fc6f5a8c400f1775
mairoo <jonghwa@pincoin.co.kr>
2024-12-10 20:14:28 +0900

    useCallback

--- src/hooks/useCartActions.ts
+++ src/hooks/useCartActions.ts
@@ -1,21 +1,24 @@
 import { removeItem, updateItemQuantity } from '@/store/cart/slice';
 import { useAppDispatch } from '@/store/hooks';
+import { useCallback } from 'react';
 
 export const useCartActions = () => {
   const dispatch = useAppDispatch();
 
-  const handleQuantityChange = (
-    productId: number,
-    delta: number,
-    currentQuantity: number,
-  ) => {
-    const newQuantity = Math.max(1, currentQuantity + delta);
-    dispatch(updateItemQuantity({ productId, quantity: newQuantity }));
-  };
+  const handleQuantityChange = useCallback(
+    (productId: number, delta: number, currentQuantity: number) => {
+      const newQuantity = Math.max(1, currentQuantity + delta);
+      dispatch(updateItemQuantity({ productId, quantity: newQuantity }));
+    },
+    [dispatch],
+  );
 
-  const handleRemove = (productId: number) => {
-    dispatch(removeItem(productId));
-  };
+  const handleRemove = useCallback(
+    (productId: number) => {
+      dispatch(removeItem(productId));
+    },
+    [dispatch],
+  );
 
   return { handleQuantityChange, handleRemove };
 };
