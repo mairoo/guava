48452bc3ebe086098a1583fb48dbdd5b577c397f
mairoo <jonghwa@pincoin.co.kr>
2024-12-14 00:16:06 +0900

    auth provider loading

--- src/providers/auth/AuthProvider.tsx
+++ src/providers/auth/AuthProvider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { LoadingMessage } from '@/components/message';
 import { useAuth } from '@/hooks/useAuth';
 import { useLogout } from '@/hooks/useLogout';
 import { useMounted } from '@/hooks/useMounted';
@@ -102,7 +103,10 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
   if (!isMounted || isLoading) {
     return (
       <div className="min-h-screen" aria-hidden="true">
-        {children}
+        <LoadingMessage
+          message="대한민국 1등 상품권 쇼핑몰 핀코인"
+          description="잠시만 기다려주세요."
+        />
       </div>
     );
   }
